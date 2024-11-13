df60b3265a5628eed60a51e6f6c96335931003e2
SvandaPetr <Svandapetr@natur.cuni.cz>
2024-11-13 15:29:21 +0100

    text

--- poznamky ke 3 dni.R
+++ poznamky ke 3 dni.R
@@ -22,3 +22,20 @@ usethis::create_from_github(
 #a commit je record of a change a ty presne jsou zapisovany, ty zmeny ktere delame.mame soubor a udelame zmenu ulozime a to se ulozi jako commit 1, dalsi pridam dalsi text commit 2 pak za dva dny pridam obrazek a ulozi se commit 3.
 
 # soubor muze mít 3 stavy: modified: změněný soubor, staged: jestli se změna pošle a bude z ni commit nebo ne a commit: že je zaznamenána jeho změna
+
+
+#git je jako skritek co ti kouka naprojekt a kdykoliv se uvnitr vytvori zmena, tu zaznamena oproti puvodnimu souboru.
+
+#clovek pote muze jim sledovane zmeny nalozit ->stage a poté spolecne s komentarem o jakou zmenu slo commitnout. a ta zmena spolecne s tim komentem see poté zobrazí v histoii zmen.
+
+
+#je super commitovat vzdycky po nejakych logickych blocich, ne ze napisu celou vec za den a dam jeden commit. ale treba napisu jeden logicky odstavec ten commitnu o cem odstavec je treba. pak pracuju dal. aby zmeny davali smysl a jejich commity taky, kdyz si to treba po mesici otevru.
+
+#SHA je specificky kod commitu a lze to sledovat v historii
+# git vzdy porovnava soubor s pedchozi verzi a rozbaruje rozdili mezi starou a novou verzi
+# v Rstudiu vzdy zobrazujr zmeny v ramci celeho projektu v jakemkoli souboru uvnitr, ale v githubu si pote lze navolit i sledovani zmen pouze urciteho souboru.
+
+
+#zatím jsme to ukládali na local repository
+#ted, ale potrebujeme synchronizovat local a repote repository usethis::use_github tak synchronizuju git s githubem
+
